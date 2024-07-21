6c87a2f3058c3977946deb4fe1555e06f032d672
SvenRehm <rehm21@web.de>
2024-07-21 15:02:55 +0200

    added harpoon

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,4 @@
 require("config.lazy")
+
 vim.api.nvim_set_hl(0, "FlashMatch", { bg = "none", fg = "white" })
 vim.api.nvim_set_hl(0, "FlashLabel", { bg = "none", fg = "red" })
--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,3 +2,28 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "<leader>gs", ":Neogit<CR>", { desc = "Open Neogit" })
+
+--harpoon
+vim.keymap.del("n", "<leader>l")
+local harpoon = require("harpoon")
+harpoon:setup()
+
+vim.keymap.set("n", "<leader>a", function()
+  harpoon:list():add()
+end)
+vim.keymap.set("n", "<leader>l", function()
+  harpoon.ui:toggle_quick_menu(harpoon:list())
+end)
+
+vim.keymap.set("n", "<leader>n", function()
+  harpoon:list():select(1)
+end)
+vim.keymap.set("n", "<leader>e", function()
+  harpoon:list():select(2)
+end)
+vim.keymap.set("n", "<leader>i", function()
+  harpoon:list():select(3)
+end)
+vim.keymap.set("n", "<leader>o", function()
+  harpoon:list():select(4)
+end)
