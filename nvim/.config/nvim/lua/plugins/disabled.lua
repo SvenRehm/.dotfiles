de3df326cdfa81b0a7d6653ee2d5dd32ffc9eddd
SvenRehm <rehm21@web.de>
2024-08-05 20:12:15 +0200

    add supamavens

--- nvim/.config/nvim/lua/plugins/disabled.lua
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -4,4 +4,18 @@ return {
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
   { "folke/noice.nvim", enabled = false },
   -- bufferline.nvim
+  {
+    "L3MON4D3/LuaSnip",
+    keys = {
+      { "<tab>", false, mode = { "i", "s" } },
+      { "<s-tab>", false, mode = { "i", "s" } },
+    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    keys = {
+      { "<tab>", false, mode = { "i", "s" } },
+      { "<s-tab>", false, mode = { "i", "s" } },
+    },
+  },
 }
---
+++ nvim/.config/nvim/lua/plugins/supamavens.lua
@@ -0,0 +1,18 @@
+return {
+  "supermaven-inc/supermaven-nvim",
+  config = function()
+    require("supermaven-nvim").setup({
+      keymaps = {
+        accept_suggestion = "<Tab>",
+        clear_suggestion = "<C-]>",
+        accept_word = "<C-j>",
+      },
+    })
+    local cmp = require("cmp")
+    cmp.setup({
+      sources = {
+        { name = "supermaven" },
+      },
+    })
+  end,
+}
