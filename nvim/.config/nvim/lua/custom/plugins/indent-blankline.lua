3eb3d4d7b925bb0424adc972c69d3e94965ffa94
SvenRehm <rehm21@web.de>
2025-07-20 10:55:51 +0200

    indent blank line added

---
+++ nvim/.config/nvim/lua/custom/plugins/indent-blankline.lua
@@ -0,0 +1,19 @@
+return {
+  -- 'lukas-reineke/indent-blankline.nvim',
+  -- main = 'ibl',
+  -- -@module "ibl"
+  -- @type ibl.config
+  -- opts = {
+  -- scope = {
+  -- char = '│',
+  --   show_start = false,
+  --   show_end = false
+  -- },
+  -- indent = {
+  --   char = ''
+  -- char = '│',
+  -- tab_char = '│',
+  -- },
+  -- scope = { show_start = false, show_end = false },
+  -- },
+}
